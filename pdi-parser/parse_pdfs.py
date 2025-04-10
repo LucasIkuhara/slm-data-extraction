efad148e663c31a0f8795ce583d3ca2706599d55
Lucas Ikuhara <lri2911@gmail.com>
2025-04-10 14:13:50 -0300

    feat: add file functions

--- pdi-parser/parse_pdfs.py
+++ pdi-parser/parse_pdfs.py
@@ -1,35 +1,14 @@
 # %%
-from io import IOBase
-from utils import url_to_buffer
+from utils import url_to_buffer, pdf_buffer_to_txt_file
 
 # Get file names and urls
 with open("pdf_urls.txt", "r") as fd:
     file_urls = [x for x in fd.readlines() if len(x) > 0]
-    names_and_urls = list(map(lambda x: (x.split("/")[-1][:-4], x), file_urls))
+    names_and_urls = list(
+        map(lambda x: (x.split("/")[-1][:-5], x.replace("\n", "")), file_urls)
+    )
 
 # %%
 for name, url in names_and_urls:
     pdf_buffer = url_to_buffer(url)
-
-# %%
-# Get PDFs from url as Buffer
-
-# %%
-import PyPDF2
-
-
-# with open("raw-pdis/pdi_santos.pdf", "rb") as pdf_file:
-def pdf_buffer_to_txt_file(buffer: IOBase, file_name: str):
-    print(f"Writing to {file_name} from buffer...")
-    pdf = PyPDF2.PdfReader(buffer)
-    total_pages = len(pdf.pages)
-    with open(file_name, "w") as fd:
-        for idx, page in enumerate(pdf.pages):
-            print(
-                f"Writing page: {page.indirect_ref.idnum} ({100 * (idx + 1)/ total_pages:.2f}%)",
-            )
-            fd.write(page.extract_text())
-    print(f"{file_name} created successfully")
-
-
-# %%
+    pdf_buffer_to_txt_file(pdf_buffer, f"../txt/{name}.txt")
--- pdi-parser/utils.py
+++ pdi-parser/utils.py
@@ -1,5 +1,6 @@
 from requests import get
-from io import BytesIO
+from io import BytesIO, IOBase
+import PyPDF2
 
 
 def url_to_buffer(url: str) -> BytesIO:
@@ -10,15 +11,37 @@ def url_to_buffer(url: str) -> BytesIO:
     Parameters:
      - url: The base file url
 
+    Raises:
+     - In case the request fails.
+
     Returns:
      A BytesIO buffer created from the response.
     """
 
-    try:
-        response = get(url)
-    except Exception as e:
-        print(f"Failed to download file from {url}: ", e)
+    response = get(url)
+    if response.status_code != 200:
+        raise Exception(f"Request returned an error status code {response.status_code}")
 
     buffer = BytesIO(response._content)
     buffer.seek(0)
     return buffer
+
+
+def pdf_buffer_to_txt_file(buffer: IOBase, file_path: str):
+    """
+    Reads PDF buffer and extracts its text. The result is saved to a file.
+
+    Parameters:
+     - buffer: A binary buffer containing a PDF file.
+     - file_name: The output file path.
+    """
+    print(f"Writing to {file_path} from buffer...")
+    pdf = PyPDF2.PdfReader(buffer)
+    total_pages = len(pdf.pages)
+    with open(file_path, "w") as fd:
+        for idx, page in enumerate(pdf.pages):
+            print(
+                f"Writing page: {page.indirect_ref.idnum} ({100 * (idx + 1)/ total_pages:.2f}%)",
+            )
+            fd.write(page.extract_text())
+    print(f"{file_path} created successfully")
