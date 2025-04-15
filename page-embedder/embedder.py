94518f9b13c7e436f6255ca3f0c79b53370c9263
Lucas Ikuhara <ikuhara@ibm.com>
2025-04-15 17:09:16 -0300

    feat: add embedding script

--- page-embedder/embedder.py
+++ page-embedder/embedder.py
@@ -4,23 +4,29 @@ from sqlalchemy import create_engine
 from os import environ
 from embed_exporter import EmbedExporter
 import numpy as np
+from txt_reader import get_files
 
 
 conn_str = environ.get("DB_CONN_STRING")
 if conn_str is None:
 	raise Exception("Missing database connection string. Please add the DB_CONN_STRING environment variable and try again.")
 
-engine = create_engine(conn_str, echo=True)
-
+engine = create_engine(conn_str)
 with engine.connect() as conn:
+
+    # Register pg vector types
     register_vector(conn, True)
+
     exporter = EmbedExporter(conn)
+    files = get_files()
 
+    for file_name, page, raw_txt in files:
+        emb = embed(
+            model="llama3",
+            input=[raw_txt],
+        )
+        print(f"Saving embedding for {file_name} page {page}")
+        embedding_vec = np.array(emb.embeddings[0])
+        exporter.save_embedding(file_name, page, embedding_vec)
 
-    emb = embed(
-        model="llama3",
-        input=[
-            "different test embed",
-        ],
-    )
-    embedding = np.array(emb.embeddings[0])
+    print("Done exporting data.")
---
+++ page-embedder/txt_reader.py
@@ -0,0 +1,29 @@
+import os
+from typing import Tuple
+
+
+BASE_PATH = os.environ.get("BASE_PATH")
+if BASE_PATH is None:
+    raise Exception("Missing BASE_PATH. Please set this environment variable before trying again.")
+
+def get_files() -> Tuple[str, int, str]:
+    """
+    Get all available raw text files.
+
+    Returns:
+        A tuple containing file_name, page number and raw text.
+    """
+    dirs = os.listdir(BASE_PATH)
+    
+    for txt_dir in dirs:
+        files = os.listdir(f"{BASE_PATH}/{txt_dir}")
+        for txt_file in files:
+            page = int(txt_file.split(".")[0])
+            
+            with open(f"{BASE_PATH}/{txt_dir}/{txt_file}", "r") as f:
+                txt = f.read()
+                return (txt_file, page, txt)
+
+            
+        
+    
