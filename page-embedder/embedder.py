c6885241d64804442b49a54d2a0b1048d0fa2a5b
Lucas Ikuhara <ikuhara@ibm.com>
2025-04-15 15:19:44 -0300

    feat: add some embedding example

--- page-embedder/embedder.py
+++ page-embedder/embedder.py
@@ -1,7 +1,9 @@
 from ollama import embed
 from pgvector.psycopg2 import register_vector
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from os import environ
+import numpy as np
+
 
 conn_str = environ.get("DB_CONN_STRING")
 
@@ -9,17 +11,28 @@ if conn_str is None:
 	raise Exception("Missing database connection string. Please add the DB_CONN_STRING environment variable and try again.")
     
 engine = create_engine(conn_str, echo=True)
-conn = engine.connect()
 
-register_vector(conn, True)
+with engine.connect() as conn:
+    register_vector(conn, True)
 
+    emb = embed(
+        model="llama3",
+        input=[
+            "different test embed",
+        ],
+    )
+    embedding = np.array(emb.embeddings[0])
+    print(embedding)
 
-emb = embed(
-    model="llama3",
-    input=[
-        "test embed",
-    ],
-)
-embedding = emb.embeddings[0]
-conn.execute('INSERT INTO EMBEDDINGS (source_file, page_num, embedding) VALUES (%s, %s, %s)', ("a_true.pdf", 45, embedding))
+    res = conn.execute(
+        text("INSERT INTO EMBEDDING (source_file, page_num, embedding) VALUES (:some_file, :page_num, :emb)"),
+        [{'some_file': "some.pdf", "page_num": 11, "emb": embedding}]
+    )
+    conn.commit()
+    # res = conn.execute(
+    # 	text('SELECT * FROM EMBEDDING ORDER BY embedding <-> :v LIMIT 5'),
+    #     # "INSERT INTO EMBEDDINGS (source_file, page_num, embedding) VALUES ('some.pdf', 10, %s)",
+    # 	  [(embedding,)]
+    # )
+    print(res)
 
