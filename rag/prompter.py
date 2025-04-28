c0e70fdb473d75d31bab4f159e857f326bc47562
Lucas Ikuhara <lri2911@gmail.com>
2025-04-28 15:58:10 -0300

    feat: update extract data script to use new prompter

--- rag/prompter.py
+++ rag/prompter.py
@@ -16,19 +16,19 @@ class PromptBuilder:
 
         return ctx_txt
 
-    def make_prompt(self) -> str:
+    def make_prompt(self, ctx_prompt: str) -> str:
         template = """
         Dadas as informações de contexto acima, retorne uma estimativa para as
         variáveis Distância da Costa (distCosta), Comprimento Total do Duto em Km (compTotalDuto), 
         Número de Tramos (numTramos), Massa Linear de Aço em kg por Metro (massaLinearAco), 
         Massa Linear de Polímero em kg por Metro (massaLinearPolimero); no formato JSON seguindo
         o exemplo {
-            "distCosta": number,
-            "compTotalDuto": number,
-            "numTramos": number,
-            "massaLinearAco": number,
-            "massaLinearPolimero": number,
+            "distCosta": 12,
+            "compTotalDuto": 10,
+            "numTramos": 3,
+            "massaLinearAco": 40,
+            "massaLinearPolimero": 30,
         }
         """
 
-        return self.load_context("test") + template
+        return self.load_context(ctx_prompt) + template
--- rag/slm_extractor/extract_data.py
+++ rag/slm_extractor/extract_data.py
@@ -1,6 +1,6 @@
 from ollama import chat, ChatResponse
 from prompter import PromptBuilder
-from rag import Rag
+from rag.rag import Rag
 from pgvector.psycopg2 import register_vector
 from sqlalchemy import create_engine
 from os import environ
@@ -8,39 +8,36 @@ from embedding_store import EmbeddingStore
 from page_reader import PageReader
 
 
-CONTEXT_DIR = "../pdi-txt"
-msg = PromptBuilder(f"{CONTEXT_DIR}/pdi-fpso-p-32.txt").make_prompt()
-
-response: ChatResponse = chat(
-    model="llama3",
-    messages=[
-        {
-            "role": "user",
-            "content": msg,
-        },
-    ],
-)
-print(response.message.content)
-
 conn_str = environ.get("DB_CONN_STRING")
 model = environ.get("EMBEDDING_MODEL")
 BASE_PATH = environ.get("BASE_PATH")
+MODEL = "llama3"
 
 if not all([conn_str, model, BASE_PATH]):
     raise Exception(
         "Missing environment variables. Please set DB_CONN_STRING, EMBEDDING_MODEL, BASE_PATH and try again."
     )
 
-
+# Create db connection
 engine = create_engine(conn_str)
 with engine.connect() as conn:
-
-    page_reader = PageReader(BASE_PATH)
-
-    # Register pg vector types
     register_vector(conn, True)
-    store = EmbeddingStore(conn)
 
-    rag = Rag(store, page_reader, "llama3")
-    ctx = rag.get_context_by_prompt("Animais marinhos", ["pdi-fpso-p-32"])
-    print(ctx[0])
+    # Initialize components
+    store = EmbeddingStore(conn)
+    page_reader = PageReader(BASE_PATH)
+    rag = Rag(store, page_reader, MODEL)
+    prompter = PromptBuilder("pdi-fpso-p-32.txt", rag)
+
+    # Prompt model
+    msg = prompter.make_prompt("A distância da costa medida em kilometros")
+    response: ChatResponse = chat(
+        model=MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": msg,
+            },
+        ],
+    )
+    print(response.message.content)
