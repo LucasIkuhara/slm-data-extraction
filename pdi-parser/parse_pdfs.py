dd333ac863aa525b7d85121b180e78e4e60122b3
Lucas Ikuhara <lri2911@gmail.com>
2025-05-27 19:55:47 -0300

    feat: improve extraction logic

--- pdi-parser/parse_pdfs.py
+++ pdi-parser/parse_pdfs.py
@@ -5,14 +5,17 @@ from utils import url_to_md
 with open("pdf_urls.txt", "r") as fd:
     file_urls = [x for x in fd.readlines() if len(x) > 0]
     file_urls = [x for x in file_urls if "#" not in x]
-    names_and_urls = list(
-        map(lambda x: (x.split("/")[-1][:-5], x.replace("\n", "")), file_urls)
-    )
+
+names_and_urls = []
+for url in file_urls:
+    name = url.split("/")[-1][:-5]
+    clean_url = url.replace("\n", "")
+    names_and_urls.append((name, clean_url))
 
 # %%
 OUTPUT_DIR = "../plain-pages"
 
 for name, url in names_and_urls:
-    pdf_buffer = url_to_md(url, f"{OUTPUT_DIR}/{name}")
+    url_to_md(url, f"{OUTPUT_DIR}/{name}")
 
 # %%
