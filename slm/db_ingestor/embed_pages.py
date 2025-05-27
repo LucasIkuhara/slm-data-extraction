79313c9ed084cbb92b7d8d43cf9ded823d1dbf8e
Lucas Ikuhara <lri2911@gmail.com>
2025-05-27 20:15:20 -0300

    feat: add multi model embed

--- slm/db_ingestor/embed_pages.py
+++ slm/db_ingestor/embed_pages.py
@@ -8,12 +8,12 @@ from ollama import ResponseError
 
 
 conn_str = environ.get("DB_CONN_STRING")
-model = environ.get("EMBEDDING_MODEL")
+model_list = environ.get("EMBEDDING_MODELS").split(";")
 BASE_PATH = environ.get("PAGES_PATH")
 
-if not all([conn_str, model, BASE_PATH]):
+if not all([conn_str, model_list, BASE_PATH]):
     raise Exception(
-        "Missing environment variables. Please set DB_CONN_STRING, EMBEDDING_MODEL, BASE_PATH and try again."
+        "Missing environment variables. Please set DB_CONN_STRING, EMBEDDING_MODELS, BASE_PATH and try again."
     )
 
 
@@ -28,24 +28,27 @@ with engine.connect() as conn:
     exporter = EmbeddingStore(conn)
     files = text_reader.get_files()
 
-    for txt_file in files:
+    for model in model_list:
+        print(f"Embedding model: {model}\n\n")
 
-        print(f"Embedding page {txt_file.page} from {txt_file.source_document}")
-        try:
-            embedding_vec = embed_str(txt_file.raw_text, model)
-            exporter.save_embedding(
-                txt_file.source_document, txt_file.page, embedding_vec, model
-            )
-        except exc.IntegrityError:
-            print(
-                f"Page embedding for {txt_file.source_document} page {txt_file.page} already exists. Skipping..."
-            )
-            conn.rollback()
-            continue
-        except ResponseError as err:
-            print(
-                f"Page embedding for {txt_file.source_document} page {txt_file.page} failed:",
-                err,
-            )
-            continue
+        for txt_file in files:
+            print(f"Embedding page {txt_file.page} from {txt_file.source_document}")
+
+            try:
+                embedding_vec = embed_str(txt_file.raw_text, model)
+                exporter.save_embedding(
+                    txt_file.source_document, txt_file.page, embedding_vec, model
+                )
+            except exc.IntegrityError:
+                print(
+                    f"Page embedding for {txt_file.source_document} page {txt_file.page} already exists. Skipping..."
+                )
+                conn.rollback()
+                continue
+            except ResponseError as err:
+                print(
+                    f"Page embedding for {txt_file.source_document} page {txt_file.page} failed:",
+                    err,
+                )
+                continue
     print("Done exporting data.")
