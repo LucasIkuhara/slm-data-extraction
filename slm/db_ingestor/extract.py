68a6457d1b92fe85c3d81a7c47fb5c2fd645bc6e
Lucas Ikuhara <lri2911@gmail.com>
2025-11-26 09:31:35 -0300

    fix: new chain not being created per document

--- slm/db_ingestor/extract.py
+++ slm/db_ingestor/extract.py
@@ -2,13 +2,15 @@ from db_ingestor.chains import make_rag_chain
 from db_ingestor.config import cfg
 import pandas as pd
 
-
-document = ["pdf-fpso-cidade-de-santos"]
-chain = make_rag_chain(cfg["system-prompt"], document)
 extracted = []
 
 for document in cfg["documents"]:
+
+    # Create a chain with rag only containing this doc
+    print("Current chain: ", document)
+    chain = make_rag_chain(cfg["system-prompt"], [document])
     for quest in cfg["questions"]:
+
         prompt = quest["prompt"]
         eq = quest["equipment"]
 
@@ -18,4 +20,4 @@ for document in cfg["documents"]:
         extracted.append(ext)
 
 df = pd.DataFrame(extracted, columns=["Document", "Equipment", "Question", "Response"])
-df.to_csv("out.csv", index=False)
+df.to_csv("out2.csv", index=False)
