6927124392aad80d425b9d2bdf46cc3c8a60e97a
Lucas Ikuhara <lri2911@gmail.com>
2025-11-27 21:23:52 -0300

    feat: correct output formatting

--- slm/db_ingestor/extract_data.py
+++ slm/db_ingestor/extract_data.py
@@ -15,7 +15,6 @@ extracted = []
 
 def enhance_prompt(base: str, field: str) -> str:
     json_template = """
-    Caso a resposta seja verdadeiro ou falso, responda 1 para verdadeiro e 0 para falso.
     Responda em json seguindo o formato: 
     {
         valor: number,
@@ -25,8 +24,7 @@ def enhance_prompt(base: str, field: str) -> str:
     return (base + json_template).replace("{campo}", field)
 
 
-for basin, field in targets:
-
+def extract_col_by_field(field: str, basin: str) -> tuple:
     # Get relevant documents by basin - field
     docs = meta_db.execute(
         "SELECT document FROM METADATA WHERE BACIA = ? AND CAMPO = ?",
@@ -38,25 +36,39 @@ for basin, field in targets:
 
     # Create a chain with rag only containing these docs
     chain = make_json_rag_chain(cfg["system-prompt"], docs)
+
+    ext = [basin, field]
     for quest in cfg["questions"]:
 
         prompt = enhance_prompt(quest["prompt"], field)
         var = quest["var"]
 
-        print(f"Extracting {var}: {prompt}")
+        print(f"Extracting {var}: {quest['prompt']}")
         resp = chain.invoke({"input": prompt})
         resp_obj = json.loads(resp["answer"])
 
         # Append extraction
-        ext = (basin, field, var, prompt, resp_obj["valor"], resp_obj["fonte"])
-        extracted.append(ext)
+        ext += [resp_obj["valor"], resp_obj["fonte"]]
+    return tuple(ext)
+
+
+for basin, field in targets:
+    try:
+        field_cols = extract_col_by_field(field, basin)
+        extracted.append(field_cols)
+    except Exception as err:
+        print(f"Warning! Skipped {basin} - {field}. Results will be incomplete")
+        print(err)
+        continue
 
 # Save results
 today = datetime.now()
 dir_name = f"results/{today.strftime('%Y-%m-%d')}"
 Path(dir_name).mkdir(parents=True, exist_ok=True)
 file_path = f"{dir_name}/raw_{today.isoformat()}.csv"
-df = pd.DataFrame(
-    extracted, columns=["Bacia", "Campo", "Coluna", "Prompt", "Valor", "Fonte"]
-)
+
+cols = ["Bacia", "Campo"]
+for quest in cfg["questions"]:
+    cols += [quest["var"], quest["var"] + "_src"]
+df = pd.DataFrame(extracted, columns=cols)
 df.to_csv(file_path, index=False)
