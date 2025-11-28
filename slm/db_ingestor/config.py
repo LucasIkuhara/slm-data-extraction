da89e8f4c855c42e13b94149b102b02ab39e7804
Lucas Ikuhara <lri2911@gmail.com>
2025-11-27 23:37:03 -0300

    feat: adaptable cfg

--- slm/db_ingestor/config.py
+++ slm/db_ingestor/config.py
@@ -2,7 +2,8 @@ from yaml import safe_load
 from os import listdir
 
 with open("db_ingestor/config.yml") as yml:
-    cfg = safe_load(yml)
-
-cfg["documents"] = listdir(cfg["txt-docs-dir"])
+    cfg: dict = safe_load(yml)
 assert cfg, "Missing config.yml"
+
+if not cfg.get("documents"):
+    cfg["documents"] = listdir(cfg["txt-docs-dir"])
