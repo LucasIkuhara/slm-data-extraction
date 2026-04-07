7004ad8ae17c9574948ddf17f2f8eb146e0810a0
Lucas Ikuhara <lri2911@gmail.com>
2026-04-06 23:48:32 -0300

    feat: add validation matching

--- validation/validation.py
+++ validation/validation.py
@@ -2,10 +2,15 @@
 import pandas as pd
 
 # %%
+# Read and format Extracted Df
 ext_df = pd.read_csv("../slm/results/latest.csv")
+ext_df["BC_CMP"] = ext_df["Bacia"].str.upper() + ":" + ext_df["Campo"].str.upper()
+ext_df["BC_CMP"] = ext_df["BC_CMP"].str.strip()
+
 print(ext_df.head())
 
 # %%
+# Read and format Ground Truth Df
 gt_df = pd.read_excel("ground_truth.xlsx", sheet_name="Data (2)", header=2)
 
 # Column mapping
@@ -17,13 +22,24 @@ col_map = {
     "SKIDS_QNT": "qtd_skid",
     "LDA": "lamina",
 }
-
 gt_df = gt_df.rename(columns=col_map)
+
 gt_df["Bacia"] = gt_df["Bacia"].str.replace("BACIA DE", "").str.replace("BACIA", "")
+gt_df["BC_CMP"] = gt_df["Bacia"] + ":" + gt_df["Campo"]
+gt_df["BC_CMP"] = gt_df["BC_CMP"].str.strip()
 print(gt_df.head())
 
 
 # %%
+# Iterate through extracted and compare
+for field in ext_df["BC_CMP"].unique():
+    gt = gt_df[gt_df["BC_CMP"] == field.strip()]
 
+    if not len(gt):
+        print(f"[WARN]: field {field} not found in Ground Truth dataset.")
+        continue
+
+    print(field, len(gt))
+results = []
 
 # %%
